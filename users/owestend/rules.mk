e98396ecbc58bb53c1fb3f7635b3bad873ce87da
Ole Westendorff <owestend@gmail.com>
2020-09-18 08:59:25 +0200

    owestend: rearange users/owestend/rules.mk

--- users/owestend/rules.mk
+++ users/owestend/rules.mk
@@ -1,16 +1,3 @@
-SRC += 	owestend.c \
-		process_records.c
-
-# enable secrets
-ifneq ($(strip $(NO_SECRETS)), yes)
-    ifneq ("$(wildcard $(USER_PATH)/secrets.c)","")
-        SRC += secrets.c
-    endif
-    ifeq ($(strip $(NO_SECRETS)), lite)
-        OPT_DEFS += -DNO_SECRETS
-    endif
-endif
-
 # Enable Bootmagic Lite to consistently reset to bootloader and clear EEPROM.
 BOOTMAGIC_ENABLE = lite
 
@@ -24,9 +11,6 @@ EXTRAKEY_ENABLE = yes
 # reports a keyboard or mouse device.)
 NKRO_ENABLE = yes
 
-# Enable link-time optimization to reduce binary size.
-LINK_TIME_OPTIMIZATION_ENABLE = yes
-
 # Disable unused build options on all keyboards.
 COMMAND_ENABLE = no
 CONSOLE_ENABLE = no
@@ -36,3 +20,20 @@ TERMINAL_ENABLE = no
 # Disable all LED functions, cause i don't like it.
 BACKLIGHT_ENABLE = no      # Dissable keyboard backlight functionality
 RGBLIGHT_ENABLE = no       # Dissable keyboard RGB underglow
+
+ifeq ($(strip $(KEYBOARD)), xd68)
+endif
+
+SRC += 	owestend.c \
+		process_records.c
+
+# enable secrets
+ifneq ($(strip $(NO_SECRETS)), yes)
+    ifneq ("$(wildcard $(USER_PATH)/secrets.c)","")
+        SRC += secrets.c
+    endif
+    ifeq ($(strip $(NO_SECRETS)), lite)
+        OPT_DEFS += -DNO_SECRETS
+    endif
+endif
+

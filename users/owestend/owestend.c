65d273850619a6a1e61cd1065110d42f38658a7b
Ole Westendorff <owestend@gmail.com>
2020-09-20 16:03:11 +0200

    owestend: add new secrec keycodes

--- users/owestend/owestend.c
+++ users/owestend/owestend.c
@@ -29,10 +29,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FNC] = TEMPLATE_TKL(\
         RESET  ,          RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,RGB_M_SN,RGB_M_K, RGB_M_X, RGB_M_G, _______, _______, _______, _______,   _______, _______, _______,
-        _______, KC_SEC1, KC_SEC2, KC_SEC3, KC_SEC4, KC_SEC5, KC_SEC6, KC_SEC7, KC_SEC8, KC_SEC9, KC_SEC0, _______, _______, _______,   _______, _______, _______,
+        _______, SEC_M(0),SEC_M(1),SEC_M(2),SEC_M(3),SEC_M(4),SEC_M(5),SEC_M(6),SEC_M(7),SEC_M(8),SEC_M(9),_______, _______, _______,   _______, _______, _______,
         BL_TOGG, BL_INC  ,BL_DEC , BL_STEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
         RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______, _______, _______, _______, _______,          _______,
-        VLK_TOG,          RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,          _______,            KC_PGUP, 
+        VLK_TOG,          RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,          _______,            KC_PGUP,
         _______, _______, _______,                   KC_SPC2, KC_SPC1, KC_SPC3,                   MO_ADJ , _______, _______, _______,   KC_HOME, KC_PGDN, KC_END),
 
     [_ADJ] = TEMPLATE_TKL(\
@@ -44,32 +44,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                   KC_SPC2, KC_SPC1, KC_SPC3,                   _______, _______, _______, _______,   _______, _______, _______)
 
 };
-
-
-/*
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BL] = LAYOUT_65_ansi(
-        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,   KC_HOME,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,   KC_PGUP,
-        LCTL_ESC,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,    KC_PGDN,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,     KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(_FL), KC_RCTL, KC_LEFT,   KC_DOWN,   KC_RGHT
-    ),
-
-    [_FL] = LAYOUT_65_ansi(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL,    KC_INS,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,   _______,
-        RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, _______, _______,            _______,   _______,
-        _______,          RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,   KC_PGUP,   _______,
-        _______, _______, _______,                            MO(_CL),                   _______, _______, _______, KC_HOME,   KC_PGDN,   KC_END
-    ),
-
-    [_CL] = LAYOUT_65_ansi(
-        _______,KC_SEC_1,KC_SEC_2,KC_SEC_3,KC_SEC_4,KC_SEC_5,KC_SEC_6,KC_SEC_7,KC_SEC_8,KC_SEC_9,KC_SEC_10,_______, _______,   _______,   _______,
-        _______, _______, _______, EEP_RST, RESET,   _______, _______, _______, _______, _______, _______, _______, _______,   _______,   _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______,   _______,
-        _______,          _______, _______, _______, KC_VRSN, _______, _______, KC_MAKE, _______, _______, _______, _______,   _______,   _______,
-        _______, _______, _______,                            _______,                   _______, _______, _______, _______,   _______,   _______
-    ),TE_&%
-};
-*/

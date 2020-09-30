285820f0130fea38abff0a6dc90af184adfeb0b6
Ole Westendorff <owestend@gmail.com>
2020-09-30 15:46:14 +0200

    owestend: 65_ansi change to default tada68 layout

--- layouts/community/65_ansi/owestend/config.h
+++ layouts/community/65_ansi/owestend/config.h
@@ -13,9 +13,9 @@
     K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3D,        K75,      \
     K40, K41, K42,           K45, K46, K47,           K4A, K4B, K4C, K4D,   K76, K77, K78  \
 ) LAYOUT_65_ansi( \
-    K50, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,      K64,     \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,      K65,     \
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D,      K68,     \
-    K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3D, K75, K67,     \
+    K50, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,      K00,     \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,      K66,     \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D,      K65,     \
+    K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3D, K75, K68,     \
     K40, K41, K42,                K46,                K4A, K4B, K4D, K76, K77, K78      \
 )
